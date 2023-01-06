7250fdff586d8ce142ae66377bb6b6c5a7bc8f0c
MariaMantusova <maria.mantusova@mail.ru>
2023-01-06 22:07:19 +0300

    feat: add security

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const rateLimit = require('express-rate-limit');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
 
 const app = express();
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
 
 const userRouter = require('./routes/users');
 const cardRouter = require('./routes/cards');
@@ -22,6 +28,7 @@ app.use((req, res, next) => {
 });
 
 app.use(helmet());
+app.use(limiter);
 app.use(bodyParser.json());
 app.use('/', userRouter);
 app.use('/', cardRouter);
