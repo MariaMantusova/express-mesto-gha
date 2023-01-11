49333fbf01784615ef45b586f6b410a60be9e5cf
MariaMantusova <maria.mantusova@mail.ru>
2023-01-11 23:28:03 +0300

    feat: changes

--- models/user.js
+++ models/user.js
@@ -6,14 +6,10 @@ const AuthorizationError = require('../errors/AuthorizationError');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    minlength: 2,
-    maxlength: 30,
     default: 'Жак-Ив Кусто',
   },
   about: {
     type: String,
-    minlength: 2,
-    maxlength: 30,
     default: 'Исследователь',
   },
   avatar: {
