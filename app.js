90f024dea78aebeb3b94e654943f9a21c75b36b7
MariaMantusova <maria.mantusova@mail.ru>
2023-01-11 23:24:03 +0300

    feat: name about

--- app.js
+++ app.js
@@ -29,12 +29,16 @@ app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
+    name: Joi.string().required().min(2).max(30),
+    about: Joi.string().min(2).max(30),
   }),
 }), login);
 app.post('/signup', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
+    name: Joi.string().required().min(2).max(30),
+    about: Joi.string().min(2).max(30),
   }),
 }), createUser);
 app.use('/', userRouter);
