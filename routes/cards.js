b4a713525d8c30ffc1820587ab676181f3b12c9b
MariaMantusova <maria.mantusova@mail.ru>
2023-01-13 19:10:10 +0300

    feat: add joi for card

--- routes/cards.js
+++ routes/cards.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const {
   getCards, deleteCard, createCard, likeCard, dislikeCard,
 } = require('../controllers/cards');
@@ -6,11 +7,28 @@ const { throwEror } = require('../utils/throwError');
 const auth = require('../middlewares/auth');
 
 router.get('/cards', auth, getCards);
-router.post('/cards', auth, createCard);
+router.post('/cards', auth, celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    link: Joi.string().min(2).pattern(/https?:\/\/[a-zA-Z0-9\S]+/),
+  }),
+}), createCard);
 
-router.delete('/cards/:cardId', auth, deleteCard);
-router.put('/cards/:cardId/likes', auth, likeCard);
-router.delete('/cards/:cardId/likes', auth, dislikeCard);
+router.delete('/cards/:cardId', auth, celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().alphanum().length(24),
+  }),
+}), deleteCard);
+router.put('/cards/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().alphanum().length(24),
+  }),
+}), auth, likeCard);
+router.delete('/cards/:cardId/likes', auth, celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().alphanum().length(24),
+  }),
+}), dislikeCard);
 router.all('*', throwEror);
 
 module.exports = router;
