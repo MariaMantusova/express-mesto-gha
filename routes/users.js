f40ceed5433a624f2094f1d95cb1f4bade2f689f
MariaMantusova <maria.mantusova@mail.ru>
2023-01-13 18:45:30 +0300

    feat: add joi for params

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const {
   getUsers, getUserById, changeInfo, changeAvatar, getCurrentUser,
 } = require('../controllers/users');
@@ -6,7 +7,11 @@ const auth = require('../middlewares/auth');
 
 router.get('/users', auth, getUsers);
 router.get('/users/me', auth, getCurrentUser);
-router.get('/users/:userId', auth, getUserById);
+router.get('/users/:userId', auth, celebrate({
+  params: Joi.object().keys({
+    userId: Joi.string().alphanum().length(24),
+  }),
+}), getUserById);
 
 router.patch('/users/me', auth, changeInfo);
 router.patch('/users/me/avatar', auth, changeAvatar);
